28e7b088b36a5e9f53185394db5bdba8ea89276c
Leonardo-Pike-Excell <leonardopike.excell@gmail.com>
2026-04-08 16:54:40 +1000

    feat: add shortcut

--- source/keymaps.py
+++ source/keymaps.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import platform
+
 import bpy
 
 addon_keymaps = []
@@ -9,7 +11,16 @@ def register() -> None:
     assert bpy.context
     if kc := bpy.context.window_manager.keyconfigs.addon:
         km = kc.keymaps.new(name='Node Editor', space_type="NODE_EDITOR")
+
         kmi = km.keymap_items.new("node.na_recenter_selected", type='G', value='PRESS', alt=True)
+        addon_keymaps.append((km, kmi))
+
+        if platform.system() != 'Darwin':
+            kmi = km.keymap_items.new(
+              "node.na_arrange_selected", type='A', value='PRESS', shift=True, ctrl=True)
+        else:
+            kmi = km.keymap_items.new(
+              "node.na_arrange_selected", type='A', value='PRESS', shift=True, oskey=True)
         addon_keymaps.append((km, kmi))
 
 
